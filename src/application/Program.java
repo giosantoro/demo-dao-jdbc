9b307b061cb0092e79ab3b1836a48d9f305f3d0e
Giovanni Santoro <giggio.santoro@gmail.com>
2021-04-10 11:46:58 -0300

    Department created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
 	}
 
 }
