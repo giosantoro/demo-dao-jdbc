f6c78d37585bdbd4f720656d235f420da01708b8
Giovanni Santoro <giggio.santoro@gmail.com>
2021-04-10 14:38:36 -0300

    Seller update()

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,8 +33,16 @@ public class Program {
 		
 		System.out.println("\n=== Test 4: Seller.insert() ====");
 		Seller newSeller = new Seller(null, "Greg Cyan", "greg@gmail.com", new Date(), 4000.0, dep);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: Seller.update() ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Brown");
+		seller.setEmail("martha@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed: " + seller);
+		
 	}
 
 }
